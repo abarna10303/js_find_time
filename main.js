6b5967ce4a66a6354beaaedc93fc21234b2da8de
abarna10303 <abarnas361@gmail.com>
2022-12-27 16:43:42 +0530

    Modify

--- main.js
+++ main.js
@@ -1,18 +1,21 @@
 let date=new Date();
 let gettime=parseInt(prompt("Enter the time"));
 let newTime=date.setTime(gettime);
-if(newTime<=11 && newTime>=5)
+if(newTime<12 && newTime>=5)
 {
     document.write("Morning");
 }
-else if(newTime<=16 && newTime>=12)
+else if(newTime<16 && newTime>=12)
 {
     document.write("Afternoon");
 }
-else if(newTime<=19 && newTime>=17)
+else if(newTime<=19 && newTime>=16)
 {
     document.write("Evening");
 }
-else{
+else if(newTime<=24 && newTime>=20||newTime<5){
     document.write("Night");
+}
+else{
+    document.write("Please Enter the Correct Time")
 }
